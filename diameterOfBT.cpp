3f21496cefd09fc0acd14ef73cb1a92af5bbae44
Abhilash <abhilashjha335@gmail.com>
2022-11-08 13:44:28 +0530

    return diameter or width of B.T

---
+++ diameterOfBT.cpp
@@ -0,0 +1,74 @@
+/*
+The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two end nodes. The diagram below shows two trees each with diameter nine, the leaves that form the ends of the longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes). 
+
+Example 1:
+
+Input:
+       1
+     /  \
+    2    3
+Output: 3
+Example 2:
+
+Input:
+         10
+        /   \
+      20    30
+    /   \ 
+   40   60
+Output: 4
+Your Task:
+You need to complete the function diameter() that takes root as parameter and returns the diameter.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= Number of nodes <= 10000
+1 <= Data of a node <= 1000
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+class Solution{
+    public:
+     pair<int, int> diamtr(Node* root){
+        //base case
+        if(root == NULL){
+            pair<int, int> p = make_pair(0,0);
+            return p;
+        }
+
+        pair<int, int> left = diamtr(root->left);
+        pair<int, int> right = diamtr(root->right);
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second + 1;
+
+        pair<int, int> ans;
+        ans.first = max(op1, max(op2, op3));
+        ans.second = max(left.second , right.second) + 1;
+
+        return ans;
+     }
+
+     // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root){
+       
+       return diamtr(root).first;
+    }
+};
